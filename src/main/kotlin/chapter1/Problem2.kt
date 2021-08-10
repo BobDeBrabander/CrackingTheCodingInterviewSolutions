9fdb66a6623a100e5cf5c475c08f2ee10f71a6d5
bdebrabander <bdebrabander@bol.com>
2021-08-10 13:03:24 +0200

    1.2 fast hashMap solution

--- src/main/kotlin/chapter1/Problem2.kt
+++ src/main/kotlin/chapter1/Problem2.kt
@@ -3,6 +3,18 @@ package chapter1
 object Problem2 {
 
     fun String.permutationOf(otherString: String) : Boolean {
-        return this.toCharArray().sorted() == otherString.toCharArray().sorted()
+        val hashMap = HashMap<Char, Int>()
+        this.forEach { c ->
+            hashMap[c] = hashMap[c]?.let{it + 1} ?: 1
+        }
+        otherString.forEach { c ->
+            val hashMapVal = hashMap[c]
+            if (hashMapVal == null) return false
+            else {
+                if (hashMapVal == 1) hashMap.remove(c)
+                else hashMap[c] = hashMapVal - 1
+            }
+        }
+        return true
     }
 }
