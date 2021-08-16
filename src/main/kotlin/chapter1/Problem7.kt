f0ce0f6127be36e1579f5a4b00b757a9e066affa
BobDeBrabander <b.d.brabander@student.tue.nl>
2021-08-16 18:39:19 +0200

    skeleton problem 1.7 and test

---
+++ src/main/kotlin/chapter1/Problem7.kt
@@ -0,0 +1,10 @@
+package chapter1
+
+object Problem7 {
+
+    data class Matrix(val rows: MutableList<MutableList<Short>>)
+
+    fun Matrix.rotateMatrix(){
+        throw NotImplementedError()
+    }
+}
---
+++ src/test/kotlin/chapter1/Problem7Tests.kt
@@ -0,0 +1,28 @@
+package chapter1
+
+import chapter1.Problem7.rotateMatrix
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class Problem7Tests {
+
+    @Test
+    fun `simple matrix rotation`(){
+        val row1 = mutableListOf<Short>(1, 2, 3)
+        val row2 = mutableListOf<Short>(4, 5, 6)
+        val row3 = mutableListOf<Short>(7, 8, 9)
+        val matrix = Problem7.Matrix(mutableListOf(row1, row2, row3))
+
+        val outRow1 = mutableListOf<Short>(7, 4, 1)
+        val outRow2 = mutableListOf<Short>(8, 5, 2)
+        val outRow3 = mutableListOf<Short>(9, 6, 3)
+        val outMatrix = Problem7.Matrix(mutableListOf(outRow1, outRow2, outRow3))
+
+        handleTestCase(matrix, outMatrix)
+    }
+
+    fun handleTestCase(inputMatrix: Problem7.Matrix, expectedOutput: Problem7.Matrix){
+        var outMatrix = inputMatrix.apply{this.rotateMatrix()}
+        assertEquals(expectedOutput, outMatrix)
+    }
+}
