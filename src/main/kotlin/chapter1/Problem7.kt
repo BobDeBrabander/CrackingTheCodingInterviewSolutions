d38f27f6b93617c09d2b517f3f22a04b1f694868
bdebrabander <bdebrabander@bol.com>
2021-08-16 19:12:01 +0200

    implemented solution to 1.7

--- src/main/kotlin/chapter1/Problem7.kt
+++ src/main/kotlin/chapter1/Problem7.kt
@@ -2,9 +2,38 @@ package chapter1
 
 object Problem7 {
 
-    data class Matrix(val rows: MutableList<MutableList<Short>>)
+    data class Matrix(val rows: MutableList<MutableList<Short>>, val size: Int)
 
-    fun Matrix.rotateMatrix(){
-        throw NotImplementedError()
+    fun Matrix.rotateMatrix() {
+        for (i in 0..((size - 1) / 2)) {
+            for (j in i..((size - 1) / 2)) {
+                val cell1 = CellIndex(i, j)
+                val cell2 = cell1.get90Cell(size)
+                val cell3 = cell2.get90Cell(size)
+                val cell4 = cell3.get90Cell(size)
+                this.swap(cell1, cell2)
+                this.swap(cell2, cell3)
+                this.swap(cell3, cell4)
+                // cell1 -> cell 4
+                //  ^          |
+                //  |          v
+                // cell 2 <- cell 3
+            }
+        }
+    }
+
+    data class CellIndex(val row: Int, val col: Int){
+        //row,col => -col, row
+        fun get90Cell(size: Int): CellIndex{
+            val newRow = size - 1 - col
+            val newCol = row
+            return CellIndex(newRow, newCol)
+        }
+    }
+
+    fun Matrix.swap(i1: CellIndex, i2: CellIndex) {
+        val cell2 = this.rows[i2.row][i2.col]
+        this.rows[i2.row][i2.col] = this.rows[i1.row][i1.col]
+        this.rows[i1.row][i1.col] = cell2
     }
 }
--- src/test/kotlin/chapter1/Problem7Tests.kt
+++ src/test/kotlin/chapter1/Problem7Tests.kt
@@ -11,12 +11,12 @@ class Problem7Tests {
         val row1 = mutableListOf<Short>(1, 2, 3)
         val row2 = mutableListOf<Short>(4, 5, 6)
         val row3 = mutableListOf<Short>(7, 8, 9)
-        val matrix = Problem7.Matrix(mutableListOf(row1, row2, row3))
+        val matrix = Problem7.Matrix(mutableListOf(row1, row2, row3), 3)
 
         val outRow1 = mutableListOf<Short>(7, 4, 1)
         val outRow2 = mutableListOf<Short>(8, 5, 2)
         val outRow3 = mutableListOf<Short>(9, 6, 3)
-        val outMatrix = Problem7.Matrix(mutableListOf(outRow1, outRow2, outRow3))
+        val outMatrix = Problem7.Matrix(mutableListOf(outRow1, outRow2, outRow3), 3)
 
         handleTestCase(matrix, outMatrix)
     }
