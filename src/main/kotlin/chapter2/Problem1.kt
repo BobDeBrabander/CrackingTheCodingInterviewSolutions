4a991b52fe1c0b411f10f184aed6a3a3019d3bf4
bdebrabander <bdebrabander@bol.com>
2021-08-22 12:08:13 +0200

    problem 2.1 solved

--- src/main/kotlin/chapter2/Problem1.kt
+++ src/main/kotlin/chapter2/Problem1.kt
@@ -1,10 +1,22 @@
 package chapter2
 
-import java.util.LinkedList
+import mydatastructures.MyLinkedList
 
 object Problem1 {
 
-    fun LinkedList<Int>.removeDuplicates() : LinkedList<Int>{
+    fun MyLinkedList<Int>.removeDuplicates() : MyLinkedList<Int>{
+        val seen = HashSet<Int>()
+        var curr = this.head
+        while(curr?.value != null){
+            if (seen.contains(curr.value)) {
+                val next = curr.next
+                this.unlink(curr)
+                curr = next
+                continue
+            }
+            else seen.add(curr.value!!)
+            curr = curr.next
+        }
         return this
     }
 }
--- src/test/kotlin/chapter2/Problem1Tests.kt
+++ src/test/kotlin/chapter2/Problem1Tests.kt
@@ -1,25 +1,25 @@
 package chapter2
 
 import chapter2.Problem1.removeDuplicates
+import mydatastructures.MyLinkedList
 import org.junit.Test
-import java.util.LinkedList
 import kotlin.test.assertEquals
 
 class Problem1Tests {
 
     @Test
     fun `simple test`(){
-        val ll: LinkedList<Int> = LinkedList()
+        val ll: MyLinkedList<Int> = MyLinkedList()
         ll.addAll(listOf(6,1,2,3,1,3,4,5))
 
-        val outll: LinkedList<Int> = LinkedList()
+        val outll: MyLinkedList<Int> = MyLinkedList()
         outll.addAll(listOf(6,1,2,3,4,5))
 
         handleTestCase(ll, outll)
     }
 
-    fun handleTestCase(inputLL: LinkedList<Int>, expectedOutputLL: LinkedList<Int>){
-        assertEquals(inputLL.removeDuplicates(), expectedOutputLL)
+    fun handleTestCase(inputLL: MyLinkedList<Int>, expectedOutputLL: MyLinkedList<Int>){
+        assertEquals(inputLL.removeDuplicates().toList(), expectedOutputLL.toList())
     }
 
 }
