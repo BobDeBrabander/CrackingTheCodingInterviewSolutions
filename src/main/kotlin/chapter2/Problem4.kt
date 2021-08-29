ad1241f6d25d5c4529efc9a6ddf6bd0224562c1e
BobDeBrabander <b.d.brabander@student.tue.nl>
2021-08-29 12:07:19 +0200

    Problem 2.4 solved

--- src/main/kotlin/chapter2/Problem4.kt
+++ src/main/kotlin/chapter2/Problem4.kt
@@ -4,7 +4,20 @@ import mydatastructures.MySinglyLinkedList
 
 object Problem4 {
 
-    fun partitionAround(ll: MySinglyLinkedList<Int>, partition: Int): MySinglyLinkedList<Int>{
-        throw NotImplementedError()
+    fun partitionAround(ll: MySinglyLinkedList<Int>, partition: Int): MySinglyLinkedList.Node<Int>?{
+        var curNode = ll.head
+        var head : MySinglyLinkedList.Node<Int>? = null
+        var tail : MySinglyLinkedList.Node<Int>? = null
+        while (curNode != null){
+            val newNode = MySinglyLinkedList.Node<Int>(curNode.value)
+            if (curNode.value!! < partition){
+                newNode.next = head
+                head = newNode
+            } else {
+                tail?.next = newNode
+            }
+            curNode = curNode.next
+        }
+        return head
     }
 }
--- src/test/kotlin/chapter2/Problem4Tests.kt
+++ src/test/kotlin/chapter2/Problem4Tests.kt
@@ -12,14 +12,17 @@ class Problem4Tests {
         val ll = MySinglyLinkedList<Int>()
         ll.addAll(listOf(3, 5, 8, 5, 10, 2, 1))
         handleTestCase(5, ll)
+        handleTestCase(3, ll)
+        handleTestCase(8, ll)
     }
 
     private fun handleTestCase(partition: Int, ll: MySinglyLinkedList<Int>) {
-        val output = partitionAround(ll, partition)
-        var node = output.head
+        val outputHead = partitionAround(ll, partition)
+        var node = outputHead
         var seenPartition = false
         while (true) {
             when {
+                node == null -> break
                 (node?.value!! > partition) || (node?.value!! < partition && !seenPartition) -> node = node.next
                 (node?.value!! == partition) -> seenPartition = true
                 (node?.value!! < partition && seenPartition) -> fail("since we have seen the partition element we shouldn't see elements smaller than the partition anymore")
