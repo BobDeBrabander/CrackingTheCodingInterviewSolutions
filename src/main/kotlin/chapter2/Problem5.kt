d2b5eaa989d919b891a33a1658af05ca1b70b73d
BobDeBrabander <b.d.brabander@student.tue.nl>
2021-08-29 12:26:57 +0200

    Problem 2.5 solved forward + additional tests

--- src/main/kotlin/chapter2/Problem5.kt
+++ src/main/kotlin/chapter2/Problem5.kt
@@ -4,6 +4,20 @@ import mydatastructures.MySinglyLinkedList
 
 object Problem5 {
     fun sumListsForward(ll1: MySinglyLinkedList<Int>, ll2: MySinglyLinkedList<Int>) : MySinglyLinkedList<Int>{
-        throw NotImplementedError()
+        var cur1 = ll1.head
+        var cur2 = ll2.head
+        var output = MySinglyLinkedList<Int>()
+
+        var takeOver = 0
+        while(cur1 != null || cur2 != null){
+            val totalValue = (cur1?.value ?: 0) + (cur2?.value ?: 0) + takeOver
+            takeOver = if (totalValue > 10) 1 else 0
+            val value = if (totalValue >= 10) totalValue - 10 else totalValue
+            output.add(value)
+            if (cur1 != null) cur1 = cur1.next
+            if (cur2 != null) cur2 = cur2.next
+        }
+        if (takeOver != 0) output.add(takeOver)
+        return output
     }
 }
--- src/test/kotlin/chapter2/Problem5Tests.kt
+++ src/test/kotlin/chapter2/Problem5Tests.kt
@@ -18,6 +18,28 @@ class Problem5Tests {
         llOut.addAll(listOf(2, 1, 9))
     }
 
+    @Test
+    fun `simple test 2 `(){
+        val ll1 = MySinglyLinkedList<Int>()
+        ll1.addAll(listOf(7, 1, 6, 8))
+        val ll2 = MySinglyLinkedList<Int>()
+        ll2.addAll(listOf(5, 9, 2))
+
+        val llOut = MySinglyLinkedList<Int>()
+        llOut.addAll(listOf(2, 1, 9, 8))
+    }
+
+    @Test
+    fun `simple test 3`(){
+        val ll1 = MySinglyLinkedList<Int>()
+        ll1.addAll(listOf(7, 1, 7))
+        val ll2 = MySinglyLinkedList<Int>()
+        ll2.addAll(listOf(5, 8, 2))
+
+        val llOut = MySinglyLinkedList<Int>()
+        llOut.addAll(listOf(2, 0, 0, 1))
+    }
+
     fun handleTestCase(ll1: MySinglyLinkedList<Int>, ll2: MySinglyLinkedList<Int>, expect: MySinglyLinkedList<Int>){
         assertEquals(sumListsForward(ll1, ll2).toList(), expect.toList())
     }
