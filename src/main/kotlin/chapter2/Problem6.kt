ea1f325147253e26c7bc08de2aab031912ecf951
BobDeBrabander <b.d.brabander@student.tue.nl>
2021-09-05 13:26:54 +0200

    2.6 solved

--- src/main/kotlin/chapter2/Problem6.kt
+++ src/main/kotlin/chapter2/Problem6.kt
@@ -5,6 +5,21 @@ import mydatastructures.MySinglyLinkedList
 object Problem6 {
 
     fun isPalindrome(ll: MySinglyLinkedList<Int>) : Boolean{
-        throw NotImplementedError()
+        var nodeRev : MySinglyLinkedList.Node<Int>? = null
+        var curNode = ll.head
+        while(curNode != null){
+            var newNode = MySinglyLinkedList.Node(curNode.value)
+            newNode.next = nodeRev
+            nodeRev = newNode
+            curNode = curNode.next
+        }
+        //At this point we should have one reversed version of the list starting with nodeRev
+        var nodeNorm = ll.head
+        while (nodeNorm != null && nodeRev != null){
+            if (nodeNorm.value != nodeRev.value) return false
+            nodeNorm = nodeNorm.next
+            nodeRev = nodeRev.next
+        }
+        return true
     }
 }
