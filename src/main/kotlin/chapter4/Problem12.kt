e46bd60c613c0ac5295ba6d93eab8b75ad21a02f
BobDeBrabander <b.d.brabander@student.tue.nl>
2021-11-14 19:45:15 +0100

    4.12 Corrected: Smarter faster solution

--- src/main/kotlin/chapter4/Problem12.kt
+++ src/main/kotlin/chapter4/Problem12.kt
@@ -5,7 +5,7 @@ object Problem12 {
     class BinaryTree {
         var root: BinaryTreeNode? = null
 
-        fun numberOfPathsSumTo(sum: Int) = root?.numberOfPathsSumTo(sum) ?: 0
+        fun numberOfPathsSumTo(sum: Int) = root?.numberOfPathsSumTo(sum, 0, HashMap()) ?: 0
     }
 
     class BinaryTreeNode(val data: Int, val depth: Int) {
@@ -13,15 +13,27 @@ object Problem12 {
         var leftChild: BinaryTreeNode? = null
         var rightChild: BinaryTreeNode? = null
 
-        fun numberOfPathsSumTo(sum: Int) = numberOfPathsSumTo(mutableListOf(), sum)
+        fun numberOfPathsSumTo(targetSum: Int, currentSum: Int, pathCount: HashMap<Int, Int>) : Int{
+            var newCurrentSum = currentSum + data
+            var sum = newCurrentSum - targetSum
+            var totalPaths = pathCount.getOrDefault(sum, 0)
+            if (newCurrentSum == targetSum) totalPaths++
 
-        private fun numberOfPathsSumTo(possibleSumsTillNow: MutableList<Int>, sum: Int): Int {
-            var possibleSumsTillNowCopy = possibleSumsTillNow.map { it + data }.toMutableList()
-            possibleSumsTillNowCopy.add(data)
-            val sumsToSumHere = possibleSumsTillNowCopy.count { it == sum }
-            val sumsLeft = if (leftChild != null) leftChild!!.numberOfPathsSumTo(possibleSumsTillNowCopy, sum) else 0
-            val sumsRight = if (rightChild != null) rightChild!!.numberOfPathsSumTo(possibleSumsTillNowCopy, sum) else 0
-            return sumsToSumHere + sumsLeft + sumsRight
+            pathCount.increment(newCurrentSum, 1)
+            totalPaths += leftChild?.numberOfPathsSumTo(targetSum, newCurrentSum, pathCount) ?: 0
+            totalPaths += rightChild?.numberOfPathsSumTo(targetSum, newCurrentSum, pathCount) ?: 0
+            pathCount.increment(newCurrentSum, -1)
+
+            return totalPaths
+        }
+
+        fun HashMap<Int, Int>.increment(key: Int, delta: Int){
+            val newCount = this.getOrDefault(key, 0) + delta
+            if (newCount == 0){
+                this.remove(key)
+            } else {
+                this[key] = newCount
+            }
         }
     }
 }
