e0d78e642f3fbd25f7a9c90d430c8ec6f536d0bc
BobDeBrabander <b.d.brabander@student.tue.nl>
2021-11-15 01:01:41 +0100

    5.7 solved

---
+++ src/main/kotlin/chapter5/Problem7.kt
@@ -0,0 +1,15 @@
+package chapter5
+
+object Problem7 {
+
+    //Odd and even bits have to be swapped:
+    //e.g. bit 0 and 1 are swapped, bit 2 and 3 are swapped an so on
+    //As few instructions as possible
+    fun pairWiseSwap(A: Int) : Int{
+        val maskOddBits = 1431655765    //01010101010101010101010101010101
+        val maskEvenBits = -1431655766  //10101010101010101010101010101010
+        val oddBits = A and maskOddBits
+        val evenBits = A and maskEvenBits
+        return (oddBits shl 1) xor (evenBits shr 1)
+    }
+}
---
+++ src/test/kotlin/chapter5/Problem7Tests.kt
@@ -0,0 +1,27 @@
+package chapter5
+
+import chapter5.Problem7.pairWiseSwap
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class Problem7Tests {
+
+    @Test
+    fun `Simple testcase`(){
+        val A = 29        //00011101
+        val expect = 46   //00101110
+        runTest(A, expect)
+    }
+
+    @Test
+    fun `Big testcase`(){
+        val A = 1431655765        //01010101010101010101010101010101
+        val expect = -1431655766  //10101010101010101010101010101010
+        runTest(A, expect)
+    }
+
+
+    fun runTest(A: Int, expect: Int){
+        assertEquals(expect, pairWiseSwap(A))
+    }
+}
