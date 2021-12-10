a93bc847d2c150e5dd947fbf279512b1befa41b8
BobDeBrabander <b.d.brabander@student.tue.nl>
2021-12-10 20:49:29 +0100

    Problem 16.16 Corrected from solutions.

--- src/main/kotlin/chapter16/Problem16.kt
+++ src/main/kotlin/chapter16/Problem16.kt
@@ -2,18 +2,52 @@ package chapter16
 
 object Problem16 {
 
-    fun subSort(list: List<Int>): Pair<Int, Int> {
-        val sortedList = list.sorted()
-        var start = 0
-        while (start < list.size && list[start] == sortedList[start]) {
-            start++
-        }
-        if (start == list.size) return 0 to 0
+    fun subSort(list: List<Int>): Pair<Int, Int>? {
+        val endOfStart = endOfStartSequence(list)
+        if (endOfStart == list.size - 1) return null //already sorted
 
-        var end = list.size - 1
-        while (end > 0 && list[end] == sortedList[end]) {
-            end--
+        val startOfEnd = startOfEndSequence(list)
+        var maxIndex = endOfStart
+        var minIndex = startOfEnd
+        for (i in endOfStart + 1 until startOfEnd) {
+            if (list[i] < list[minIndex]) minIndex = i
+            if (list[i] > list[maxIndex]) maxIndex = i
         }
-        return start to end
+
+        return shrinkLeft(list, minIndex, endOfStart) to shrinkRight(list, maxIndex, startOfEnd)
+    }
+
+    private fun endOfStartSequence(list: List<Int>): Int {
+        for (i in 1 until list.size) {
+            if (list[i] < list[i - 1]) {
+                return i - 1
+            }
+        }
+        return list.size - 1
+    }
+
+    private fun startOfEndSequence(list: List<Int>): Int {
+        for (i in list.size - 2 downTo 0) {
+            if (list[i] > list[i + 1]) {
+                return i + 1
+            }
+        }
+        return 0
+    }
+
+    private fun shrinkLeft(list: List<Int>, minIndex: Int, start: Int): Int {
+        val comp = list[minIndex]
+        for (i in start - 1 downTo 0) {
+            if (list[i] <= comp) return i + 1
+        }
+        return 0
+    }
+
+    private fun shrinkRight(list: List<Int>, maxIndex: Int, end: Int): Int {
+        val comp = list[maxIndex]
+        for (i in end + 1 until list.size) {
+            if (list[i] >= comp) return i - 1
+        }
+        return list.size - 1
     }
 }
