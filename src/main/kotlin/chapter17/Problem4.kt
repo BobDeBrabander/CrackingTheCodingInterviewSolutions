ef6b8bd6aa1bf9906b6d3e983e7a7c9dc07f34ea
BobDeBrabander <b.d.brabander@student.tue.nl>
2021-12-13 20:46:26 +0100

    Chapter 17 Problem 1 - 5 Corrected: 1: Way simpler solution existed 2: Correct 3: Correct 4: Faster solution was there 5: Correct

--- src/main/kotlin/chapter17/Problem4.kt
+++ src/main/kotlin/chapter17/Problem4.kt
@@ -2,31 +2,36 @@ package chapter17
 
 object Problem4 {
 
-    class BitList(private val list: List<Int>) {
-        fun getBitOfNum(numIndex: Int, bitIndex: Int): Int {
-            return (list[numIndex] and (1 shl bitIndex)) ushr bitIndex
-        }
-
-        fun size(): Int {
-            return list.size
+    class BitInt(private val num: Int){
+        fun getBit(bitIndex: Int) : Int{
+            return (num and (1 shl bitIndex)) ushr bitIndex
         }
     }
 
-    fun missingNumber(list: BitList, n: Int): Int {
-        var numsFound = MutableList(n) { false }
-        repeat(n - 1) { index ->
-            var bit = 0
-            var curNum = 0
-            while (1 shl bit <= n) {
-                val bitFound = list.getBitOfNum(index, bit)
-                if (bitFound == 1) curNum += (1 shl bit)
-                bit++
+    fun missingNumber(list: List<BitInt>): Int {
+        return missingNumber(list, 0)
+    }
+
+    private fun missingNumber(list: List<BitInt>, column: Int): Int {
+        if (column >= 31) {
+            return 0
+        }
+        val oneBits = mutableListOf<BitInt>()
+        val zeroBits = mutableListOf<BitInt>()
+
+        list.forEach {
+            if (it.getBit(column) == 0) {
+                zeroBits.add(it)
+            } else {
+                oneBits.add(it)
             }
-            numsFound[curNum] = true
         }
-        repeat(n){
-            if (!numsFound[it]) return it
+        return if (zeroBits.size <= oneBits.size) {
+            val v = missingNumber(zeroBits, column+1)
+            (v shl 1) or 0 //Last bit is 0, v will be the other bits behind it
+        } else {
+            val v = missingNumber(oneBits, column+1)
+            (v shl 1) or 1 //Last bit is 1, v will be the other bits behind it
         }
-        return -1
     }
 }
--- src/test/kotlin/chapter17/Problem4Tests.kt
+++ src/test/kotlin/chapter17/Problem4Tests.kt
@@ -7,18 +7,16 @@ class Problem4Tests {
 
     @Test
     fun `simple test`() {
-        val list = listOf(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 15)
-        val bitList = Problem4.BitList(list)
-        val result = Problem4.missingNumber(bitList, 15)
+        val list = listOf(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 15).map{ Problem4.BitInt(it) }
+        val result = Problem4.missingNumber(list)
         val expect = 13
         assertThat(result).isEqualTo(expect)
     }
 
     @Test
     fun `simple test 2`() {
-        val list = listOf(0, 1, 2, 3, 4, 5, 6, 7, 9)
-        val bitList = Problem4.BitList(list)
-        val result = Problem4.missingNumber(bitList, 9)
+        val list = listOf(0, 1, 2, 3, 4, 5, 6, 7, 9).map{ Problem4.BitInt(it) }
+        val result = Problem4.missingNumber(list)
         val expect = 8
         assertThat(result).isEqualTo(expect)
     }
