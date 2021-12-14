0fa6251fc9490cc8e6cb6cdba6b50bec3e1c94e5
BobDeBrabander <b.d.brabander@student.tue.nl>
2021-12-14 21:00:43 +0100

    Chapter 17 Problem 6 - 7 Corrected: 6: Correct 7: Correct but made it a bit prettier

--- src/main/kotlin/chapter17/Problem7.kt
+++ src/main/kotlin/chapter17/Problem7.kt
@@ -2,27 +2,34 @@ package chapter17
 
 object Problem7 {
 
+    class FrequencyNode (val name: String, val frequency: Int){
+        var visited = false
+        val adj = mutableListOf<FrequencyNode>()
+
+        fun addLink(other: FrequencyNode) {
+            adj.add(other)
+        }
+    }
+
+    private fun componentFrequency(node: FrequencyNode) : Int {
+        if (node.visited) return 0
+        node.visited = true
+        var sum = node.frequency
+        node.adj.forEach {
+            sum += componentFrequency(it)
+        }
+        return sum
+    }
+
     fun babyNames(frequencyMap: Map<String, Int>, synonyms: List<Pair<String, String>>): Map<String, Int> {
-        val copiedFrequencyMap = frequencyMap.toMutableMap()
-        val adjacencyList = mutableMapOf<String, MutableList<String>>()
-        frequencyMap.keys.forEach { adjacencyList[it] = mutableListOf() }
-        synonyms.forEach { (name1, name2) -> adjacencyList[name1]?.add(name2) }
-
-        fun takeFrequencyUpstream(name: String): Int {
-            val ans = (adjacencyList[name]?.map { takeFrequencyUpstream(it) }?.sum() ?: 0) + copiedFrequencyMap[name]!!
-            adjacencyList.remove(name)
-            copiedFrequencyMap.remove(name)
-            return ans
+        val nodes = frequencyMap.map{ (name, frequency) -> name to FrequencyNode(name, frequency)}.toMap()
+        synonyms.forEach { (name1, name2) ->
+            nodes[name1]?.addLink(nodes[name2]!!)
+            nodes[name2]?.addLink(nodes[name1]!!)
         }
-
-        while (adjacencyList.isNotEmpty()) {
-            val parent = adjacencyList.keys.first()
-            val children = adjacencyList[parent]!!
-            children.forEach { child ->
-                copiedFrequencyMap[parent] = copiedFrequencyMap[parent]!! + takeFrequencyUpstream(child)
-            }
-            adjacencyList.remove(parent)
-        }
-        return copiedFrequencyMap
+        return nodes.values.mapNotNull {
+            if (it.visited) null
+            else it.name to componentFrequency(it)
+        }.toMap()
     }
 }
--- src/test/kotlin/chapter17/Problem7Tests.kt
+++ src/test/kotlin/chapter17/Problem7Tests.kt
@@ -2,15 +2,27 @@ package chapter17
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
+import kotlin.test.assertTrue
 
 class Problem7Tests {
 
     @Test
     fun `example test`() {
-        val frequencyMap = mapOf("John" to 15, "Jon" to 12, "Johnny" to 4, "Chris" to 13, "Kris" to 4, "Christopher" to 19)
+        val frequencyMap =
+            mapOf("John" to 15, "Jon" to 12, "Johnny" to 4, "Chris" to 13, "Kris" to 4, "Christopher" to 19)
         val synonyms = listOf("Jon" to "John", "John" to "Johnny", "Chris" to "Kris", "Chris" to "Christopher")
         val result = Problem7.babyNames(frequencyMap, synonyms)
-        val expect = mapOf("Jon" to 31, "Chris" to 36)
-        assertThat(result).isEqualTo(expect)
+
+        assertTrue(
+            (result.containsKey("Jon") && result["Jon"] == 31) ||
+                    (result.containsKey("John") && result["John"] == 31) ||
+                    (result.containsKey("Johnny") && result["Johnny"] == 31)
+        )
+        assertTrue(
+            (result.containsKey("Kris") && result["Kris"] == 36) ||
+                    (result.containsKey("Christopher") && result["Christopher"] == 36) ||
+                    (result.containsKey("Chris") && result["Chris"] == 36)
+        )
+        assertThat(result).hasSize(2)
     }
 }
