82708f86d31497a12b96d0a852b03441b0ff5a45
BobDeBrabander <b.d.brabander@student.tue.nl>
2021-12-20 20:50:30 +0100

    Chapter 17 Problem 17 - 23 Corrected: 17: Copied from solutions 18: Correct 19: Solved but better solution added for part 2 20: Skipped 21: Correct 22: Solution improved 23: Correct

--- src/main/kotlin/chapter17/Problem19.kt
+++ src/main/kotlin/chapter17/Problem19.kt
@@ -1,22 +1,46 @@
 package chapter17
 
+import kotlin.math.sqrt
+
 object Problem19 {
 
-    fun missingOne(list: List<Int>) : Int {
-        val N = list.size+1
-        val sum1toN = ((1+N)*N)/2
+    fun missingOne(list: List<Int>): Int {
+        val N = list.size + 1
+        val sum1toN = ((1 + N) * N) / 2
         return sum1toN - list.sum()
     }
 
-    fun missingTwo(list: List<Int>) : List<Int> {
-        val N = list.size+2
-        val sum1ToN = ((1+N)*N)/2
-        val listSum = list.sum()
-        val difference = sum1ToN - listSum
-        val sortedList = list.sorted()
-        for(i in sortedList.indices){
-            if (sortedList[i] != i+1) return listOf(i+1, difference-(i+1))
-        }
-        return listOf()
+    fun missingTwo(list: List<Int>): List<Int> {
+        val N = list.size + 2
+        val sum1ToN = ((1 + N) * N) / 2
+        val sumSquared1ToN = sumSquaredToN(N)
+        val differenceSum = sum1ToN - list.sum()
+        val differenceSquareSum = sumSquared1ToN - list.map { it * it }.sum()
+
+        val missingOne = solveEquation(differenceSum, differenceSquareSum)
+        val missingTwo = differenceSum - missingOne
+        return listOf(missingOne, missingTwo).sorted()
+    }
+
+    private fun sumSquaredToN(N: Int): Int {
+        return (1..N).toList().map { it * it }.sum()
+    }
+
+    private fun solveEquation(r1: Int, r2: Int): Int {
+        //r1 = x + y
+        //r2 = x^2 + y^2
+        // y = r1 - x
+        // x^2 + (r1 - x)^2 = r2
+        // x^2 + -2*x*r1 + x^2 + r1^2 = r2
+        // 2x^2 - 2*x*r1 + r1^2 - r2 = 0
+
+        // ax^2 + bx + c = 0
+        //Trying to solve for (-b += sqrt(b^2 - 4ac)) / 2a
+        //So the values of a, b, and c become =>
+        val a = 2
+        val b = -2 * r1
+        val c = r1 * r1 - r2
+
+        return ((-b + sqrt((b * b - 4 * a * c).toDouble())) / (2 * a)).toInt()
     }
 }
