3b88f5feb29c0786363f5bb28954e7bd5e4db9ab
BobDeBrabander <b.d.brabander@student.tue.nl>
2021-12-27 15:48:32 +0100

    Redid exercise 17.9 on a googleDoc:  - priorityQueue was redundant, use kotlin ArrayDeque instead

--- src/main/kotlin/chapter17/Problem9GoogleDoc.kt
+++ src/main/kotlin/chapter17/Problem9GoogleDoc.kt
@@ -1,39 +1,38 @@
 package chapter17
 
-import java.util.PriorityQueue
 import kotlin.math.min
 
 object Problem9GoogleDoc {
-    fun kthMultiple(k: Int) : Int {
+    fun kthMultiple(k: Int): Int {
         if (k < 0) return -1
         var curValue = 1
-        val q3 = PriorityQueue<Int>()
-        val q5 = PriorityQueue<Int>()
-        val q7 = PriorityQueue<Int>()
+        val q3 = ArrayDeque<Int>()
+        val q5 = ArrayDeque<Int>()
+        val q7 = ArrayDeque<Int>()
         q3.add(3)
         q5.add(5)
         q7.add(7)
 
-        repeat(k){
+        repeat(k) {
             val q3val = q3.firstOrNull() ?: Integer.MAX_VALUE
             val q5val = q5.firstOrNull() ?: Integer.MAX_VALUE
             val q7val = q7.firstOrNull() ?: Integer.MAX_VALUE
             curValue = min(q3val, min(q5val, q7val))
             when (curValue) {
                 q3val -> {
-                    q3.poll()
-                    q3.add(curValue*3)
-                    q5.add(curValue*5)
-                    q7.add(curValue*7)
+                    q3.removeFirst()
+                    q3.addLast(curValue * 3)
+                    q5.addLast(curValue * 5)
+                    q7.addLast(curValue * 7)
                 }
                 q5val -> {
-                    q5.poll()
-                    q5.add(curValue*5)
-                    q7.add(curValue*7)
+                    q5.removeFirst()
+                    q5.addLast(curValue * 5)
+                    q7.addLast(curValue * 7)
                 }
                 q7val -> {
-                    q7.poll()
-                    q7.add(curValue*7)
+                    q7.removeFirst()
+                    q7.addLast(curValue * 7)
                 }
             }
         }
